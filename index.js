0214f6e0e850f8f5d85ae7e8d2100e7b771d90f5
Brinth KR <itzbrinth@gmail.com>
2023-05-28 19:01:39 +0530

    adds fib

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
  console.log('hello');
 
  function getName(){
+     
     return 'Brinth'
  }
-
+ function fibonacci(num) 
+  {    
+       if(num==1) 
+           return 0; 
+       if (num == 2) 
+           return 1; 
+       return  fibonacci(num - 1) + fibonacci(num - 2);
+  } 
+  console.log(fibonacci(43))
  console.log(getName());
